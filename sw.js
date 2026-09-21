7a075dd5962dcc951e30cea206c34600d55a165a
takuyog <taku.yo.g@gmail.com>
2026-09-22 00:36:14 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+// sw.js (サービスワーカーファイル)
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(self.clients.claim());
+});
+
+// バックグラウンド通知受信用
+self.addEventListener('notificationclick', (event) => {
+    event.notification.close();
+    event.waitUntil(
+        clients.matchAll({ type: 'window', includeUncontrolled: true }).then((clientList) => {
+            if (clientList.length > 0) {
+                return clientList[0].focus();
+            }
+            return clients.openWindow('/');
+        })
+    );
+});
